e3ce5ecbfca1822e4efd2338ac54e4e6cf6e736e
dojdev <mr.dojdev@gmail.com>
2016-03-21 04:31:53 +0300

    posts views

--- app/write.php
+++ app/write.php
@@ -6,13 +6,11 @@ $link = 'posts/';
 $linkFull = $link . $dataDay;
 
 while($a !== "exit" . PHP_EOL){
-   echo 'пишем текст в ' . $dataDay . PHP_EOL;
    $a = readline();
       if($a=='exit'){
          exit();
       } else{
-         file_put_contents($linkFull, $a, FILE_APPEND);
-         echo 'текст сохранен ' . $dataDay . PHP_EOL;
+         file_put_contents($linkFull, $a . ' ', FILE_APPEND);
       }
 }
 exit();
--- index.php
+++ index.php
@@ -1,5 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple blog</title>
+</head>
+<body>
 <?php
 
 require_once 'app/read.php';
 
 read('posts');
+
+?>
+</body>
+</html>
